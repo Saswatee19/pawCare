b4ab7a0e310d3002ec0bf4aa707bb3093e8b5ce5
19sas <19.saswatee@gmail.com>
2026-06-27 20:42:57 +0530

    minor changes done

--- services.js
+++ services.js
@@ -22,4 +22,17 @@ let preloader = document.querySelector(".preloader");
 
 setTimeout(()=>{
     preloader.classList.add('preloader-hidden');
-}, 2000)
+}, 2000);
+
+let hamburger_icon = document.querySelector(".hamburger-icon");
+let close_icon = document.querySelector(".close-icon");
+let hamburger_text = document.querySelector(".hamburger-text");
+
+hamburger_icon.addEventListener('click', ()=>{
+    hamburger_text.classList.add('open');
+    hamburger_icon.classList.add('fade_out');
+})
+close_icon.addEventListener('click', () => {
+    hamburger_text.classList.remove('open');
+    hamburger_icon.classList.remove('fade_out');
+});
